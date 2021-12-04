cc87f94820bc7be61a8a2192b2111457c0ef40ad
iamsoumikhazra <76480123+iamsoumikhazra@users.noreply.github.com>
2021-12-04 21:24:45 +0530

    Given an array of size N containing only 0s, 1s, and 2s; sort the array in ascending order.

---
+++ array/4.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    int x;
+    cin>>x;
+    int a[x];
+
+    for (int i = 0; i < x; i++)
+    {
+        cin>>a[i];
+    }
+    
+    int zeros=0;
+    int ones=0;
+    int twos=0;
+
+    for (int i = 0; i < x; i++)
+    {
+        if(a[i]==0){
+            zeros++;
+        }
+        else if(a[i]==1){
+            ones++;
+        }
+        else if(a[i]==2){
+            twos++;
+        }
+        else{
+            continue;
+        }
+    }
+
+    
+   for (int i = 0; i < x; i++)
+   {
+       if(i<zeros){
+           cout<<0<<" ";
+       }
+       else if(i>=zeros && i<zeros+ones){
+           cout<<1<<" ";
+       }
+       else if(i>=zeros+ones && i<=zeros+ones+twos){
+           cout<<2<<" ";
+       }
+
+   }
+   
+    return 0;
+
+}
